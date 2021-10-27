425b14cc7af557bcf5e8300ab63ed9355ed77f1c
Stephen Klancher <smknight@gmail.com>
2021-10-27 15:10:17 -0700

    Add KTA 7.8

---
+++ Versions/7.8/ContainerFiles/PowershellScripts/AddAdminUser.ps1
@@ -0,0 +1,3 @@
+$Password = ConvertTo-SecureString "W0rkstation" -AsPlainText -Force
+New-LocalUser -Name KTA_Admin -Password $Password -AccountNeverExpires -FullName KTA_Admin -PasswordNeverExpires -UserMayNotChangePassword
+Add-LocalGroupMember -Group "Administrators" -Member "KTA_Admin"
